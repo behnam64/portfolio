36b1d3d3d4c7db3a4e4db9e767f94ca758a8b39d
behnam rostami <behnam.rostamy.br@gmail.com>
2019-01-15 10:05:09 +0330

    router added

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,33 @@
 <template>
   <div id="app">
-    <app-header></app-header>
+    <router-view />
+    <div id="nav">
+      <router-link to="">Home</router-link>
+      <router-link to="/omnifood">omnifood</router-link>
+    </div>
+    
   </div>
 </template>
 
 <script>
-export default {
-  components: {
-    "app-header": () => import("./components/app-header.vue"),
-    "navigation": () => import("./components/navigation.vue")
+  export default {
+    data() {
+      return {
+        bool: true
+      }
+    },
+    scripts: {
+      isOnHome() {
+        this.$route.path === "/";
+      }
+    }
   }
-}
 </script>
 
 <style lang="scss">
-@import url('https://fonts.googleapis.com/css?family=Lato:100,300,300i,400');
-@import "./styles/normalize.scss";
-@import "./styles/grid.scss";
-@import "./variables.scss";
-@import "./App.scss";
+  @import url('https://fonts.googleapis.com/css?family=Lato:100,300,300i,400');
+  @import "./styles/normalize.scss";
+  @import "./styles/grid.scss";
+  @import "./variables.scss";
+  @import "./App.scss";
 </style>
---
+++ src/views/omnifood.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <app-header></app-header>
+    </div>
+</template>
+
+<script>
+    export default {
+        components: {
+            "app-header": () => import("@/components/app-header.vue")
+        }
+    }
+</script>
